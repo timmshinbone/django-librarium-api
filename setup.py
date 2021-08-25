cefc21b98b578a6142ebf4af7dfd92cc844b5f83
Timm Schoenborn <timmschoenborn@gmail.com>
2021-08-25 15:38:25 -0400

    removes refs to template

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ with open('LICENSE.md') as f:
     LICENSE = f.read()
 
 setup(
-    name='django-auth-template',
+    name='django-librarium',
     version='0.1.0',
-    description='GA SEI Boston Django Authentication Template',
+    description='Django version of Flask app Librarium',
     long_description=README,
-    author='<author>',
-    author_email='<email>',
-    url='https://git.generalassemb.ly/ga-wdi-boston/django-auth-template',
+    author='Timm Schoenborn',
+    author_email='timmschoenborn@gmail.com',
+    url='https://github.com/timmshinbone/django-librarium-api',
     license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs'))
 )
