4fe0dba44718c6f54e168fd03f33608af1b6cab9
Timm Schoenborn <timmschoenborn@gmail.com>
2021-09-01 11:06:15 -0400

    sender can delete trade request and permissions for trade create enhanced

--- api/views/trade_views.py
+++ api/views/trade_views.py
@@ -44,13 +44,13 @@ class Trades(generics.ListCreateAPIView):
 
         trade = TradeSerializer(data=request.data['trade'])
 
-        if c_from_oid == request.user.id:
+        if((c_from_oid == request.user.id) and (c_from_oid != c_to_oid)):
           if trade.is_valid():
             trade.save()
             return Response({'trade': trade.data}, status=status.HTTP_201_CREATED)
           return Response(trade.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-          raise PermissionDenied('Unauthorized, Cant trade away a copy that aint yours')
+          raise PermissionDenied('Unauthorized, Cant trade away a copy that aint yours, and ya cant trade with yourself')
 
 
 class TradeDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -66,5 +66,17 @@ class TradeDetail(generics.RetrieveUpdateDestroyAPIView):
         # data['book'] = the_book
         return Response({'trade': data})
 
-# LAY DOWN SOME CODE FOR ACCEPT AND REJECT TRADES
-# DELETE REQUEST TO TAKE THE TRADE BACK SHOULD HAVE REQUEST OWNER PERMISSION ONLY
+    # LAY DOWN SOME CODE FOR ACCEPT AND REJECT TRADES
+    # DELETE REQUEST TO TAKE THE TRADE BACK SHOULD HAVE REQUEST OWNER PERMISSION ONLY
+        # @user_passes_test(lambda u: u.is_superuser)
+    def delete(self, request, pk):
+        """Delete request"""
+        # Locate book to delete
+        trade = get_object_or_404(Trade, pk=pk)
+
+        if((trade.from_user.id == request.user.id) or (request.user.is_superuser)):
+          # Only delete if the user sent the trade
+          trade.delete()
+          return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+          raise PermissionDenied('Unauthorized, you do not have permission to delete this trade')
